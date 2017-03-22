2e89f0272b2405f0dadb75f7fb84f203855e2ec2
Cheng-Yu Pai <mail4charliepai@gmail.com>
2017-03-22 01:45:36 -0700

    DesktopLauncher now uses config file

--- desktop/src/main/java/com/pipai/adv/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/pipai/adv/desktop/DesktopLauncher.java
@@ -1,7 +1,11 @@
 package com.pipai.adv.desktop;
 
+import java.io.File;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.files.FileHandle;
+import com.pipai.adv.AdvConfig;
 import com.pipai.adv.AdvGame;
 
 public final class DesktopLauncher {
@@ -11,9 +15,12 @@ public final class DesktopLauncher {
 
     // @cs.suppress [UncommentedMain] this is the main entry point
     public static void main(String[] arg) {
+        AdvConfig advConfig = new AdvConfig(new FileHandle(new File("config/config.properties")));
+        advConfig.writeToFile();
+
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1024;
-        config.height = 768;
+        config.width = advConfig.getResolution().getWidth();
+        config.height = advConfig.getResolution().getHeight();
         config.resizable = false;
         new LwjglApplication(new AdvGame(), config);
     }
