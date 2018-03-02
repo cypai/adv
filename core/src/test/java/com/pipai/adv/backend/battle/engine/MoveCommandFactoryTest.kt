9601a837bd4031ea80fdceb83b9ca28ab6409582
Cheng-Yu Pai <mail4charliepai@gmail.com>
2018-03-02 01:05:25 -0800

    Add MoveCommandFactoryTest

---
+++ core/src/test/java/com/pipai/adv/backend/battle/engine/MoveCommandFactoryTest.kt
@@ -0,0 +1,32 @@
+package com.pipai.adv.backend.battle.engine
+
+import com.pipai.adv.backend.battle.domain.*
+import com.pipai.adv.backend.battle.engine.commands.MoveCommandFactory
+import com.pipai.adv.npc.NpcList
+import com.pipai.adv.save.AdvSave
+import com.pipai.test.fixtures.npcFromStats
+import org.junit.Assert
+import org.junit.Test
+
+class MoveCommandFactoryTest {
+
+    @Test
+    fun testMoveFactory() {
+        val save = AdvSave()
+        val npcList = NpcList()
+        val map = BattleMap.createBattleMap(4, 4)
+        val npc = npcFromStats(UnitStats(1, 1, 1, 1, 1, 1, 1, 1, 2), null)
+        val id = npcList.addNpc(npc)
+        map.getCell(0, 0).fullEnvObject = FullEnvObject.NpcEnvObject(id, Team.PLAYER, EnvObjTilesetMetadata.NONE)
+
+        val backend = BattleBackend(save, npcList, map)
+
+        val factory = MoveCommandFactory(backend)
+        val moves = factory.generate(id)
+        Assert.assertEquals(5, moves.size)
+
+        moves.forEach {
+            Assert.assertTrue(backend.canBeExecuted(it).executable)
+        }
+    }
+}
