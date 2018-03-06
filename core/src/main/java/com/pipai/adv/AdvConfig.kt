7cfb0c0f09a4bd89fd3c32d3d6dccf1a36024c2f
Cheng-Yu Pai <mail4charliepai@gmail.com>
2018-03-06 00:19:40 -0800

    Support a few more screen resolutions

--- core/src/main/java/com/pipai/adv/AdvConfig.kt
+++ core/src/main/java/com/pipai/adv/AdvConfig.kt
@@ -3,29 +3,28 @@ package com.pipai.adv
 import com.badlogic.gdx.files.FileHandle
 import com.pipai.adv.utils.getLogger
 import com.pipai.adv.utils.valueOfOrDefault
-import java.util.Properties
+import java.util.*
 
-enum class AspectRatio {
-    AR_4_3,
-    AR_16_10,
-    AR_16_9,
-}
-
-enum class ScreenResolution(val width: Int, val height: Int, val aspectRatio: AspectRatio, val description: String?,
+enum class ScreenResolution(val width: Int, val height: Int, val description: String?,
                             val tileSize: Int, val tileOffset: Int) {
     // 4:3 Resolutions
-    RES_1024_768(1024, 768, AspectRatio.AR_4_3, null, 32, 10),
-    RES_1280_960(1280, 960, AspectRatio.AR_4_3, null, 32, 10),
+    RES_1024_768(1024, 768, null, 32, 10),
+    RES_1280_960(1280, 960, null, 32, 10),
 
-    // 16:10 Resolutions (rare but should be supported, why not)
-    RES_1280_800(1280, 800, AspectRatio.AR_16_10, null, 32, 10),
+    // 16:10 Resolutions
+    RES_1280_800(1280, 800, null, 32, 10),
+    RES_1680_1050(1680, 1050, null, 32, 10),
+
+    // ~16:9 Resolutions
+    RES_1366_768(1366, 768, "HD", 32, 10),
 
     // 16:9 Resolutions
-    RES_1280_720(1280, 720, AspectRatio.AR_16_9, "720p", 32, 10),
-    RES_1920_1080(1920, 1080, AspectRatio.AR_16_9, "1080p", 32, 10);
+    RES_1280_720(1280, 720, "720p", 32, 10),
+    RES_1600_900(1600, 900, "900p", 32, 10),
+    RES_1920_1080(1920, 1080, "1080p", 32, 10);
 
     fun toDebugString(): String {
-        return "ScreenResolution[${width}x${height}, ${aspectRatio}, ${description}, TileSize: ${tileSize}]"
+        return "ScreenResolution[${width}x${height}, ${description}, TileSize: ${tileSize}]"
     }
 }
 
