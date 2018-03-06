c7172fefdc73997dbcac266b32b26a0a37cd3f5d
Cheng-Yu Pai <mail4charliepai@gmail.com>
2018-03-05 17:02:09 -0800

    Upgrade to lwjgl3

--- desktop/src/main/java/com/pipai/adv/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/pipai/adv/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.pipai.adv.desktop;
 
-import java.io.File;
-
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.files.FileHandle;
 import com.pipai.adv.AdvConfig;
 import com.pipai.adv.AdvGame;
+
+import java.io.File;
 
 public final class DesktopLauncher {
 
@@ -18,10 +18,9 @@ public final class DesktopLauncher {
         AdvConfig advConfig = new AdvConfig(new FileHandle(new File("config/config.properties")));
         advConfig.writeToFile();
 
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = advConfig.getResolution().getWidth();
-        config.height = advConfig.getResolution().getHeight();
-        config.resizable = false;
-        new LwjglApplication(new AdvGame(advConfig), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(advConfig.getResolution().getWidth(), advConfig.getResolution().getHeight());
+        config.setResizable(false);
+        new Lwjgl3Application(new AdvGame(advConfig), config);
     }
 }
