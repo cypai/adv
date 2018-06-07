d43a819b73e2f58abe92af39eee6a6337d889ed4
Cheng-Yu Pai <mail4charliepai@gmail.com>
2018-06-06 22:12:19 -0700

    Track binds

--- core/src/main/java/com/pipai/adv/backend/battle/engine/NpcStatusState.kt
+++ core/src/main/java/com/pipai/adv/backend/battle/engine/NpcStatusState.kt
@@ -1,18 +1,18 @@
 package com.pipai.adv.backend.battle.engine
 
-import com.pipai.adv.backend.battle.engine.domain.AilmentInstance
-import com.pipai.adv.backend.battle.engine.domain.NpcStatus
-import com.pipai.adv.backend.battle.engine.domain.NpcStatusInstance
+import com.pipai.adv.backend.battle.engine.domain.*
 import com.pipai.adv.domain.NpcList
 
 class NpcStatusState(npcList: NpcList) {
 
     private val npcAilments: MutableMap<Int, AilmentInstance?> = mutableMapOf()
+    private val npcBinds: MutableMap<Int, BindStatus> = mutableMapOf()
     private val npcStatus: MutableMap<Int, MutableList<NpcStatusInstance>> = mutableMapOf()
 
     init {
         npcList.forEach {
             npcAilments[it.key] = null
+            npcBinds[it.key] = BindStatus(0, 0, 0)
             npcStatus[it.key] = mutableListOf()
         }
     }
@@ -23,6 +23,24 @@ class NpcStatusState(npcList: NpcList) {
 
     fun setNpcAilment(npcId: Int, ailment: AilmentInstance?) {
         npcAilments[npcId] = ailment
+    }
+
+    fun getNpcBind(npcId: Int, bodyPart: BodyPart): Int {
+        val status = npcBinds[npcId] ?: throw IllegalArgumentException("NPC $npcId does not exist")
+        return when (bodyPart) {
+            BodyPart.HEAD -> status.head
+            BodyPart.ARMS -> status.arms
+            BodyPart.LEGS -> status.legs
+        }
+    }
+
+    fun setNpcBind(npcId: Int, bodyPart: BodyPart, turns: Int) {
+        val status = npcBinds[npcId] ?: throw IllegalArgumentException("NPC $npcId does not exist")
+        return when (bodyPart) {
+            BodyPart.HEAD -> status.head += turns
+            BodyPart.ARMS -> status.arms += turns
+            BodyPart.LEGS -> status.legs += turns
+        }
     }
 
     fun getNpcStatus(npcId: Int): List<NpcStatusInstance> {
@@ -40,6 +58,11 @@ class NpcStatusState(npcList: NpcList) {
     fun decreaseTurnCount() {
         npcAilments.forEach { it.value?.turns?.minus(1) }
         npcAilments.filterValues { it != null }.forEach { npcId, _ -> npcAilments[npcId] = null }
+        npcBinds.forEach {
+            it.value.head = Math.max(0, it.value.head - 1)
+            it.value.arms = Math.max(0, it.value.arms - 1)
+            it.value.legs = Math.max(0, it.value.legs - 1)
+        }
         npcStatus.forEach { it.value.forEach { it.turns -= 1 } }
         npcStatus.forEach { npcId, status -> npcStatus[npcId] = status.filter { it.turns > 0 }.toMutableList() }
     }
---
+++ core/src/main/java/com/pipai/adv/backend/battle/engine/domain/BindStatus.kt
@@ -0,0 +1,3 @@
+package com.pipai.adv.backend.battle.engine.domain
+
+data class BindStatus(var head: Int, var arms: Int, var legs: Int)
